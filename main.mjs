a01ed688d01579db4bc5741b07fb67a93f544e31
totegamma <github@gammalab.net>
2024-02-15 23:19:26 +0900

    init

---
+++ main.mjs
@@ -0,0 +1,60 @@
+import os from "os";
+import { Worker } from "worker_threads";
+
+if (process.argv.length < 3) {
+    console.log("Usage: node main.mjs <target>");
+    process.exit(1);
+}
+
+const cpuCores = os.cpus().length;
+console.log(`CPU cores: ${cpuCores}`);
+
+const target = process.argv[2];
+
+if (!target.match(/^[0-9a-f]+$/)) {
+    console.log("Invalid target");
+    process.exit(1);
+}
+
+const workercount = cpuCores;
+const workers = [];
+for (let i = 0; i < workercount; i++) {
+    workers.push(new Worker("./worker.mjs"));
+}
+
+
+const attempts = new Array(cpuCores).fill(0);
+const checker = setInterval((attempts) => {
+    workers.forEach((worker) => {
+        worker.postMessage({
+            "type": "progress",
+        });
+    });
+    const totalAttempts = attempts.reduce((acc, val) => acc + val);
+    console.log(`Attempts: ${totalAttempts}`);
+}, 3000, attempts);
+
+workers.forEach((worker, i) => {
+    worker.on("message", (message) => {
+        switch (message.type) {
+            case "completed":
+                console.log(message.result);
+                workers.forEach((w) => w.postMessage({"type": "stop"}));
+                clearInterval(checker);
+                break;
+            case "progress":
+                attempts[message.workerId] = message.result;
+                break;
+            default:
+                console.log(message);
+        }
+    });
+
+    worker.postMessage({
+        "type": "start",
+        "target": target,
+        "workerId": i,
+    });
+});
+
+
---
+++ worker.mjs
@@ -0,0 +1,53 @@
+import { generateIdentity } from "@concurrent-world/client";
+import { parentPort } from 'worker_threads';
+
+
+let attempts = 0;
+let workerID = 0;
+let running = false;
+
+async function startWorker(target) {
+    while (running) {
+        attempts++;
+        const identity = generateIdentity();
+        if (identity.CCID.startsWith("CC"+target)) {
+            parentPort.postMessage({
+                'type': 'completed',
+                'result': identity,
+                'workerId': workerID
+            });
+            running = false;
+        }
+        await new Promise(resolve => setImmediate(resolve));
+    }
+    process.exit(0);
+}
+
+parentPort.on('message', (message) => {
+    switch (message.type) {
+        case 'start':
+            workerID = message.workerId;
+            if (!running) {
+                console.log(`Worker ${workerID} started`);
+                running = true;
+                startWorker(message.target);
+            }
+            break;
+        case 'progress':
+            if (running) {
+                parentPort.postMessage({
+                    'type': 'progress',
+                    'result': attempts,
+                    'workerId': workerID,
+                });
+            }
+            break;
+        case 'stop':
+            running = false;
+            break;
+        default:
+            console.log('Unknown message type: ' + message.type);
+            break;
+    }
+});
+
